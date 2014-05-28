cc50ef680f2ea4cf2ee2475b507664683b04cf35
mdfry <mdfry20@gmail.com>
2014-05-27 21:36:54 -0700

    Deleted old versions

--- using_morph_relatedness.py
+++
@@ -1,9 +0,0 @@
-'''
-Created on May 13, 2014
-
-@author: Michael
-'''
-import morph_relatedness
-
-#morph_relatedness.morph_relatedness_word('iphod', 'string_similarity', 'spelling', 'cry', 0)
-morph_relatedness.morph_relatedness_pairs('iphod', 'string_similarity', 'spelling', 'words_2_spell.txt', 'words_2_output.txt', -15)
