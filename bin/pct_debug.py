f6fdd5984902a71243096ff4e222a5d7e2f534ec
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2014-07-01 14:23:38 -0700

    Fix for pct_debug

--- bin/pct_debug.py
+++ bin/pct_debug.py
@@ -11,14 +11,15 @@ from corpustools.gui.maingui import GUI, make_menus,use_logo
 
 root = Tk()
 root.title("CorpusTools v0.15")
+logo_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'logo.ico')
 if use_logo:
     try:
-        root.wm_iconbitmap(os.path.join(os.getcwd(),'sample_logo.ico'))
+        root.wm_iconbitmap(logo_path)
     except FileNotFoundError:
         pass#if the file isn't found, don't bother
     except:
         pass
-app = GUI(root)
+app = GUI(root,os.path.dirname(logo_path))
 make_menus(root,app)
 root.bind_all('<Control-q>', app.quit)
 root.bind_all('<Control-h>', app.entropy)
