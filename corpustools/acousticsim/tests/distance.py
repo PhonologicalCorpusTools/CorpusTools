a8a8eb3950d923cc159858e3024350d0da2761f4
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2014-07-15 20:51:33 -0700

    Initial tests

--- corpustools/acousticsim/tests/distance.py
+++ corpustools/acousticsim/tests/distance.py
@@ -4,15 +4,16 @@ from numpy import array
 import unittest
 import os
 try:
-    from acousticsim.helpers.distance_functions import (dtw_distance,
+    from corpustools.acousticsim.distance_functions import (dtw_distance,
                                         generate_distance_matrix,
                                         xcorr_distance)
 except ImportError:
     import sys
-    
+
     test_dir = os.path.dirname(os.path.abspath(__file__))
-    sys.path.append(os.path.split(os.path.split(test_dir)[0])[0])
-    from acousticsim.helpers.distance_functions import (dtw_distance,
+    corpustools_path = os.path.split(os.path.split(os.path.split(test_dir)[0])[0])[0]
+    sys.path.append(corpustools_path)
+    from corpustools.acousticsim.distance_functions import (dtw_distance,
                                         generate_distance_matrix,
                                         xcorr_distance)
 
@@ -28,23 +29,21 @@ class DTWTest(unittest.TestCase):
                             [2,7,9],
                             [1,5,8],
                             [7,4,9]])
-    
+
     def test_dtw_unnorm(self):
         distmat = generate_distance_matrix(self.source, self.target)
         linghelper = dtw_distance(self.source,self.target,norm=False)
-        
+
         r_dtw_output = 31.14363
-        print(linghelper)
         self.assertTrue(abs(r_dtw_output - linghelper) < 0.01)
-    
+
     def test_dtw_norm(self):
         distmat = generate_distance_matrix(self.source, self.target)
         linghelper = dtw_distance(self.source,self.target,norm=True)
-        
+
         r_dtw_output = 3.114363
-        print(linghelper)
         self.assertTrue(abs(r_dtw_output - linghelper) < 0.01)
-        
-        
+
+
 if __name__ == '__main__':
     unittest.main()
