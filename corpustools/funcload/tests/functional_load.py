e244422799330384f736b6026c596e8fd35fc301
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2014-07-18 20:55:13 -0700

    A little work

--- corpustools/funcload/tests/functional_load.py
+++ corpustools/funcload/tests/functional_load.py
@@ -1,8 +1,25 @@
 import unittest
 
-class MinPairsTest(unittest.TestCase):
+try:
+    from corpustools.corpus.classes import Corpus, Word
+except ImportError:
+    import sys
+    import os
+    test_dir = os.path.dirname(os.path.abspath(__file__))
+    corpustools_path = os.path.split(os.path.split(os.path.split(test_dir)[0])[0])[0]
+    sys.path.append(corpustools_path)
+    from corpustools.corpus.classes import Corpus, Word
+from corpustools.funcload.functional_load import minpair_fl, deltah_fl
+
+
+class NeutralizeTest(unittest.TestCase):
     pass
-    
+
+class MinPairsTest(unittest.TestCase):
+    def setUp(self):
+        pass
+
+
 class DeltaHTest(unittest.TestCase):
     pass
 
