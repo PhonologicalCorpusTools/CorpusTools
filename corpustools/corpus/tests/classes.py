dca7045429ff0778fb40b517b298e02531367301
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2014-07-18 21:59:02 -0700

    Functional load unittests

--- corpustools/corpus/tests/classes.py
+++ corpustools/corpus/tests/classes.py
@@ -12,6 +12,27 @@ except ImportError:
     sys.path.append(corpustools_path)
     from corpustools.corpus.classes import (Word, Corpus, FeatureMatrix)
 
+def create_unspecified_test_corpus():
+    corpus_data = [{'spelling':'atema','transcription':['ɑ','t','e','m','ɑ'],'frequency':11.0},
+                    {'spelling':'enuta','transcription':['e','n','u','t','ɑ'],'frequency':11.0},
+                    {'spelling':'mashomisi','transcription':['m','ɑ','ʃ','o','m','i','s','i'],'frequency':5.0},
+                    {'spelling':'mata','transcription':['m','ɑ','t','ɑ'],'frequency':2.0},
+                    {'spelling':'nata','transcription':['n','ɑ','t','ɑ'],'frequency':2.0},
+                    {'spelling':'sasi','transcription':['s','ɑ','s','i'],'frequency':139.0},
+                    {'spelling':'shashi','transcription':['ʃ','ɑ','ʃ','i'],'frequency':43.0},
+                    {'spelling':'shisata','transcription':['ʃ','i','s','ɑ','t','ɑ'],'frequency':3.0},
+                    {'spelling':'shushoma','transcription':['ʃ','u','ʃ','o','m','ɑ'],'frequency':126.0},
+                    {'spelling':'ta','transcription':['t','ɑ'],'frequency':67.0},
+                    {'spelling':'tatomi','transcription':['t','ɑ','t','o','m','i'],'frequency':7.0},
+                    {'spelling':'tishenishu','transcription':['t','i','ʃ','e','n','i','ʃ','u'],'frequency':96.0},
+                    {'spelling':'toni','transcription':['t','o','n','i'],'frequency':33.0},
+                    {'spelling':'tusa','transcription':['t','u','s','ɑ'],'frequency':32.0},
+                    {'spelling':'ʃi','transcription':['ʃ','i'],'frequency':2.0}]
+    corpus = Corpus('test')
+    for w in corpus_data:
+        corpus.add_word(Word(**w))
+    return corpus
+
 class WordTest(unittest.TestCase):
     def setUp(self):
         self.basic = {'spelling':'test',
