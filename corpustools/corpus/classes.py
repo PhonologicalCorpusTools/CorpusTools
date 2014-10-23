366de1f05a690b3516cc03345df2c98518ad7857
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2014-10-22 23:07:37 -0700

    Add tier works again

--- corpustools/corpus/classes.py
+++ corpustools/corpus/classes.py
@@ -504,7 +504,7 @@ class Word(object):
 
         """
         matching_segs = self.transcription.match_segments(tier_segments)
-        new_tier = matching_segs
+        new_tier = Transcription(matching_segs)
         setattr(self,tier_name,new_tier)
         if tier_name not in self.tiers:
             self.tiers.append(tier_name)
@@ -768,7 +768,9 @@ class Corpus(object):
     def add_tier(self, tier_name, tier_features):
         if tier_name not in self._tiers:
             self._tiers.append(tier_name)
+        print(tier_features)
         tier_segs = self.features_to_segments(tier_features)
+        print(tier_segs)
         for word in self:
             word.add_tier(tier_name,tier_segs)
 
