caf177cf98176cedc5506828e76dcf0dae2b21c8
Blake Allen <allenblakeh@gmail.com>
2014-10-30 13:07:21 -0700

    Add command line to master

---
+++ command_line/cl_funcload.py
@@ -0,0 +1,34 @@
+import argparse
+import os
+import csv
+
+from corpustools.corpus.io import load_binary
+from corpustools.funcload.functional_load import *
+
+#### Parse command-line arguments
+parser = argparse.ArgumentParser(description = \
+         'Phonological CorpusTools: functional load CL interface')
+parser.add_argument('corpus_file_name', help='Name of corpus file')
+parser.add_argument('segment_pairs_file_name', help='Name of file with segment pairs')
+parser.add_argument('-o', '--outfile', help='Name of output file')
+
+args = parser.parse_args()
+
+
+#### Script-specific functions
+
+def read_segment_pairs(spfile):
+    text = spfile.read().strip()
+    return 
+
+
+####
+
+with open(args.segment_pairs_file_name) as segpairs_file:
+    corpus = load_binary(args.corpus_file_name)
+    segpairs = [line for line in csv.reader(segpairs_file, delimiter='\t') if len(line) > 0]
+
+    result = minpair_fl(corpus, segpairs)
+
+with open(args.outfile, 'w') as outfile:
+    outfile.write(result) # <- attention here
