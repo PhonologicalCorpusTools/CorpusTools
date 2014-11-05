ad4142d765a033095cc1c9fad1bb4c156f3fdd95
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2014-11-05 14:21:40 -0800

    Reorganized IO into a module
    Added beginnings of support for textgrids

--- corpustools/corpus/io/csv.py
+++ corpustools/corpus/io/csv.py
@@ -1,10 +1,8 @@
 from csv import DictReader, DictWriter
-import pickle
 import collections
 import re
 
 from corpustools.corpus.classes import Corpus, FeatureMatrix, Word
-from urllib.request import urlretrieve
 
 import time
 
@@ -14,69 +12,6 @@ class DelimiterError(Exception):
     """
     pass
 
-def download_binary(name,path, queue = None):
-    """
-    Download a binary file
-
-    Attributes
-    ----------
-    name : str
-        Identifier of file to download
-
-    path : str
-        Full path for where to save downloaded file
-
-    """
-    def report(blocknum, bs, size):
-        if queue is not None:
-            queue.put((blocknum * bs * 100) / size)
-    if name == 'example':
-        download_link = 'https://www.dropbox.com/s/a0uar9h8wtem8cf/example.corpus?dl=1'
-    elif name == 'iphod':
-        download_link = 'https://www.dropbox.com/s/xb16h5ppwmo579s/iphod.corpus?dl=1'
-    elif name == 'spe':
-        download_link = 'https://www.dropbox.com/s/k73je4tbk6i4u4e/spe.feature?dl=1'
-    elif name == 'hayes':
-        download_link = 'https://www.dropbox.com/s/qe9xiq4k68cp2qx/hayes.feature?dl=1'
-    filename,headers = urlretrieve(download_link,path, reporthook=report)
-    if queue is not None:
-        queue.put(-99)
-
-def load_binary(path):
-    """
-    Unpickle a binary file
-
-    Attributes
-    ----------
-    path : str
-        Full path of binary file to load
-
-    Returns
-    -------
-    Object
-        Object generated from the text file
-    """
-    begin = time.time()
-    with open(path,'rb') as f:
-        obj = pickle.load(f)
-    print("Load binary time: ",time.time()-begin)
-    return obj
-
-def save_binary(obj,path):
-    """
-    Pickle a Corpus or FeatureMatrix object for later loading
-
-    Attributes
-    ----------
-    obj : Corpus or FeatureMatrix
-        Object to save
-
-    path : str
-        Full path for where to save object
-
-    """
-    with open(path,'wb') as f:
-        pickle.dump(obj,f)
 
 def load_corpus_csv(corpus_name,path,delimiter,trans_delimiter='.', feature_system_path = '',pqueue=None,oqueue=None):
     """
@@ -111,7 +46,7 @@ def load_corpus_csv(corpus_name,path,delimiter,trans_delimiter='.', feature_syst
         as keys and a list of words containing those segments as values
 
     """
-    begin = time.time()
+    #begin = time.time()
     corpus = Corpus(corpus_name)
     if feature_system_path:
         feature_matrix = load_binary(feature_system_path)
@@ -164,99 +99,7 @@ def load_corpus_csv(corpus_name,path,delimiter,trans_delimiter='.', feature_syst
             raise(e)
 
     transcription_errors = corpus.check_coverage()
-    print("Loading time: ",time.time()-begin)
-    if pqueue is not None:
-        pqueue.put(-99)
-    if oqueue is not None:
-        oqueue.put(corpus)
-        oqueue.put(transcription_errors)
-    else:
-        return corpus,transcription_errors
-
-def load_corpus_text(corpus_name, path, delimiter, ignore_list, digraph_list,
-                    trans_delimiter='',feature_system_path='',
-                    string_type='transcription',pqueue=None,oqueue=None):
-    """
-    Load a corpus from a text file containing running text either in
-    orthography or transcription
-
-    Attributes
-    ----------
-    corpus_name : str
-        Informative identifier to refer to corpus
-
-    path : str
-        Full path to text file
-
-    delimiter : str
-        Character to use for spliting text into words
-
-    ignore_list : list of strings
-        List of characters to ignore when parsing the text
-
-    trans_delimiter : str
-        Character to use for splitting transcriptions into a list
-        of segments. If it equals '', each character in the transcription
-        is interpreted as a segment.  Defaults to '.'
-
-    feature_system_path : str
-        Full path to pickled FeatureMatrix to use with the Corpus
-
-    string_type : str
-        Specifies whether text files contains spellings or transcriptions.
-        Defaults to 'spelling'
-
-
-    Returns
-    -------
-    Corpus
-        Corpus object generated from the text file
-
-    dictionary
-        Dictionary with segments not in the FeatureMatrix (if specified)
-        as keys and a list of words containing those segments as values
-
-    """
-    word_count = collections.defaultdict(int)
-    corpus = Corpus(corpus_name)
-    corpus.custom = True
-    pattern = '|'.join(d for d in digraph_list)
-    pattern += '|\w'
-    digraph_re = re.compile(pattern)
-
-    if feature_system_path:
-        feature_matrix = load_binary(feature_system_path)
-        corpus.set_feature_matrix(feature_matrix)
-
-    trans_check = False
-    with open(path, encoding='utf-8-sig', mode='r') as f:
-        text = f.read()
-        if delimiter not in text:
-            e = DelimiterError('The delimiter specified does not create multiple words. Please specify another delimiter.')
-            if pqueue is not None:
-                pqueue.put(e)
-            else:
-                raise(e)
-
-        for line in text.splitlines():
-            if not line or line == '\n':
-                continue
-            line = line.split(delimiter)
-
-            for word in line:
-                word = word.strip()
-                word = digraph_re.findall(word)
-                word = [x for x in word if not x in ignore_list]
-                spell = ''.join(word)
-                d = {'spelling': spell, 'transcription': word}
-                word = Word(**d)
-                corpus.add_word(word, allow_duplicates=False)
-                word_count[word.spelling] += 1
-
-        for key,value in word_count.items():
-            corpus[key].frequency = value
-
-    transcription_errors = corpus.check_coverage()
+    #print("Loading time: ",time.time()-begin)
     if pqueue is not None:
         pqueue.put(-99)
     if oqueue is not None:
