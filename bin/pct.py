3396d421b2066a2d224626a895388769a3b7e9fa
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2014-11-13 15:16:44 -0800

    Build things

--- bin/pct.py
+++ bin/pct.py
@@ -1,22 +1,14 @@
+from multiprocessing import freeze_support
+import sys
+from corpustools.gui.qt.main import MainWindow,QApplication
 
-import os
-
-from tkinter import Tk
-from corpustools.gui.maingui import GUI, make_menus#,use_logo
 
 if __name__ == '__main__':
-    root = Tk()
-    root.title("CorpusTools v0.15")
-    #logo_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'logo.ico')
-    #logo_path = 'logo.ico'
-    #if use_logo:
-    #    try:
-    #        root.wm_iconbitmap(logo_path)
-    #    except FileNotFoundError:
-    #        pass#if the file isn't found, don't bother
-    #    except:
-    #        pass
-    app = GUI(root)
-    make_menus(root,app)
-    root.bind_all('<Control-q>', app.quit)
-    root.mainloop()
+    freeze_support()
+    app = QApplication(sys.argv)
+
+    main = MainWindow()
+    main.show()
+
+    sys.exit(app.exec_())
+
