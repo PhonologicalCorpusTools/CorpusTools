daef901d5f3e518d39326d5e88ef89022d23b1ef
Blake Allen <allenblakeh@gmail.com>
2014-11-25 13:55:17 -0800

    Fix boolean args in pct_funcload

--- command_line/pct_funcload.py
+++ command_line/pct_funcload.py
@@ -5,27 +5,33 @@ import csv
 from corpustools.corpus.io import load_binary
 from corpustools.funcload.functional_load import *
 
+
+#### Script-specific functions
+
+def check_bool(string):
+    if string == 'False':
+        return False
+    else:
+        return True
+
+def read_segment_pairs(spfile):
+    return spfile.read().strip()
+
+
 #### Parse command-line arguments
 parser = argparse.ArgumentParser(description = \
          'Phonological CorpusTools: functional load CL interface')
 parser.add_argument('corpus_file_name', help='Name of corpus file')
 parser.add_argument('segment_pairs_file_name', help='Name of file with segment pairs')
 parser.add_argument('-a', '--algorithm', default='minpair', help='Algorithm to use for calculating functional load: "minpair" for minimal pair count or "deltah" for change in entropy. Defaults to minpair.')
-parser.add_argument('-f', '--frequency_cutoff', default=0, help='Minimum frequency of words to consider as possible minimal pairs or contributing to lexicon entropy.')
-parser.add_argument('-r', '--relative_count', type=bool, default=True, help='For minimal pair FL: whether or not to divide the number of minimal pairs by the number of possible minimal pairs (words with either segment).')
-parser.add_argument('-d', '--distinguish_homophones', type=bool, default=False, help="For minimal pair FL: if False, then you'll count sock~shock (sock=clothing) and sock~shock (sock=punch) as just one minimal pair; but if True, you'll overcount alternative spellings of the same word, e.g. axel~actual and axle~actual. False is the value used by Wedel et al.")
+parser.add_argument('-f', '--frequency_cutoff', type=float, default=0, help='Minimum frequency of words to consider as possible minimal pairs or contributing to lexicon entropy.')
+parser.add_argument('-r', '--relative_count', type=check_bool, default=True, help='For minimal pair FL: whether or not to divide the number of minimal pairs by the number of possible minimal pairs (words with either segment).')
+parser.add_argument('-d', '--distinguish_homophones', type=check_bool, default=False, help="For minimal pair FL: if False, then you'll count sock~shock (sock=clothing) and sock~shock (sock=punch) as just one minimal pair; but if True, you'll overcount alternative spellings of the same word, e.g. axel~actual and axle~actual. False is the value used by Wedel et al.")
 parser.add_argument('-t', '--type_or_token', default='token', help='For change in entropy FL: specifies whether entropy is based on type or token frequency.')
 parser.add_argument('-o', '--outfile', help='Name of output file')
 
 args = parser.parse_args()
 
-
-#### Script-specific functions
-
-def read_segment_pairs(spfile):
-    text = spfile.read().strip()
-    return 
-print(args.relative_count)
 
 ####
 
--- command_line/pct_neighdens.py
+++ command_line/pct_neighdens.py
@@ -5,22 +5,20 @@ import csv
 from corpustools.corpus.io.binary import load_binary
 from corpustools.neighdens.neighborhood_density import neighborhood_density
 
+
 #### Parse command-line arguments
 parser = argparse.ArgumentParser(description = \
          'Phonological CorpusTools: neighborhood density CL interface')
 parser.add_argument('corpus_file_name', help='Name of corpus file')
 parser.add_argument('query', help='Name of word to query')
-# more needed
+parser.add_argument('-s', '--string_type', default = 'transcription', help="If 'spelling', will calculate neighborhood density on spelling. If 'transcription' will calculate neighborhood density on transcriptions.")
+algorithm = 'edit_distance'
+max_distance = 1
+tiername = 'transcription'
+count_what='type'
 parser.add_argument('-o', '--outfile', help='Name of output file')
 
 args = parser.parse_args()
-
-
-#### Script-specific functions
-
-def read_segment_pairs(spfile):
-    text = spfile.read().strip()
-    return 
 
 
 ####
