0aee1a31b0ab41b9403c5b9dedd222a877065b4f
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2014-12-08 01:07:13 -0800

    Analysis windows should be working again
    All of them use sequence_type instead of string_type
    File IO for neighdens works (loading list of words to calc and
    outputting a list of neighbors found)

--- corpustools/gui/qt/pdgui.py
+++ corpustools/gui/qt/pdgui.py
@@ -3,14 +3,11 @@ from .imports import *
 
 from .widgets import EnvironmentSelectWidget, SegmentPairSelectWidget, RadioSelectWidget
 
-from .windows import FunctionWorker
+from .windows import FunctionWorker, FunctionDialog
 
 from corpustools.prod.pred_of_dist import calc_prod,calc_prod_all_envs, ExhaustivityError, UniquenessError
 
 class PDWorker(FunctionWorker):
-
-    errorEncountered = Signal(object)
-
     def run(self):
         kwargs = self.kwargs
         self.results = list()
@@ -58,7 +55,7 @@ class PDWorker(FunctionWorker):
                 self.results.append(res)
         self.dataReady.emit(self.results)
 
-class PDDialog(QDialog):
+class PDDialog(FunctionDialog):
     header = ['Corpus',
                 'Tier',
                 'Sound1',
@@ -84,11 +81,10 @@ class PDDialog(QDialog):
                 ' relationships from contrast to allophony. PhD dissertation,'
                 ' The Ohio State University.')]
     def __init__(self, parent, corpus, showToolTips):
-        QDialog.__init__(self, parent)
+        FunctionDialog.__init__(self, parent, PDWorker())
 
         self.corpus = corpus
         self.showToolTips = showToolTips
-        layout = QVBoxLayout()
 
         pdFrame = QFrame()
         pdlayout = QHBoxLayout()
@@ -142,26 +138,9 @@ class PDDialog(QDialog):
 
         pdFrame.setLayout(pdlayout)
 
-        layout.addWidget(pdFrame)
+        self.layout().insertWidget(0,pdFrame)
 
-        self.selectedEnvButton = QPushButton('Calculate predictability of distribution for selected environments\n(add to current results table)')
-        self.allEnvButton = QPushButton('Calculate predictability of distribution for all environments\n(add to current results table)')
-        self.cancelButton = QPushButton('Cancel')
-        self.aboutButton = QPushButton('About this function...')
-        acLayout = QHBoxLayout()
-        acLayout.addWidget(self.selectedEnvButton)
-        acLayout.addWidget(self.allEnvButton)
-        acLayout.addWidget(self.cancelButton)
-        acLayout.addWidget(self.aboutButton)
-        self.selectedEnvButton.clicked.connect(self.selectedEnv)
-        self.allEnvButton.clicked.connect(self.allEnv)
-        self.aboutButton.clicked.connect(self.about)
-        self.cancelButton.clicked.connect(self.reject)
-
-        acFrame = QFrame()
-        acFrame.setLayout(acLayout)
-
-        layout.addWidget(acFrame)
+        self.setWindowTitle('Predictability of distribution')
 
         if self.showToolTips:
             self.segPairWidget.setToolTip(("<FONT COLOR=black>"
@@ -203,24 +182,6 @@ class PDDialog(QDialog):
                                     ' both. Each of these can be specified using either features or segments.'
             "</FONT>"))
 
-
-        self.setLayout(layout)
-
-        self.setWindowTitle('Predictability of distribution')
-
-        self.thread = PDWorker()
-        self.thread.errorEncountered.connect(self.handleError)
-
-        self.progressDialog = QProgressDialog('Calculating predictability of distribution...','Cancel',0,100, self)
-        self.progressDialog.setWindowTitle('Calculating predictability of distribution')
-        self.progressDialog.setAutoClose(False)
-        self.progressDialog.setAutoReset(False)
-        self.progressDialog.canceled.connect(self.thread.stop)
-        self.thread.updateProgress.connect(self.updateProgress)
-        self.thread.updateProgressText.connect(self.updateProgressText)
-        self.thread.dataReady.connect(self.setResults)
-        self.thread.dataReady.connect(self.progressDialog.accept)
-
     def updateProgressText(self, text):
         self.progressDialog.setLabelText(text)
         self.progressDialog.reset()
@@ -229,12 +190,7 @@ class PDDialog(QDialog):
         self.progressDialog.setValue(progress)
         self.progressDialog.repaint()
 
-    def handleError(self,error):
-        reply = QMessageBox.critical(self,
-                "Error encountered", str(error))
-        return None
-
-    def generateKwargs(self,allEnv):
+    def generateKwargs(self):
         kwargs = {}
         segPairs = self.segPairWidget.value()
         if len(segPairs) == 0:
@@ -243,18 +199,7 @@ class PDDialog(QDialog):
             return None
         kwargs['segment_pairs'] = segPairs
         envs = self.envWidget.value()
-        if allEnv and len(envs) > 0:
-            msgBox = QMessageBox(QMessageBox.Warning, "Ignoring environments",
-                    "Calculating predictability of distribution over all environments ignores any selected ones. Continue?", QMessageBox.NoButton, self)
-            msgBox.addButton("Continue", QMessageBox.AcceptRole)
-            msgBox.addButton("Abort", QMessageBox.RejectRole)
-            if msgBox.exec_() != QMessageBox.AcceptRole:
-                return None
-        elif not allEnv and len(envs) == 0:
-            reply = QMessageBox.critical(self,
-                    "Missing information", "Please specify at least one environment.")
-            return None
-        elif not allEnv:
+        if len(envs) > 0:
             kwargs['envs'] = envs
 
         kwargs['corpus'] = self.corpus
@@ -264,6 +209,18 @@ class PDDialog(QDialog):
         kwargs['type_token'] = self.typeTokenWidget.value()
         return kwargs
 
+    def calc(self):
+        kwargs = self.generateKwargs()
+        if kwargs is None:
+            return
+        self.thread.setParams(kwargs)
+        self.thread.start()
+
+        result = self.progressDialog.exec_()
+
+        self.progressDialog.reset()
+        if result:
+            self.accept()
 
     def calcSelectedPD(self):
         kwargs = self.generateKwargs(allEnv=False)
