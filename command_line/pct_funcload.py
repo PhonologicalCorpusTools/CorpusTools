b775123ab898bb04ca3906b9aa7dbb8d1c30dd7e
Blake Allen <allenblakeh@gmail.com>
2014-12-07 16:36:01 -0800

    rename `tier` in FL to `sequence_type`

--- command_line/pct_funcload.py
+++ command_line/pct_funcload.py
@@ -28,9 +28,9 @@ def main():
     parser.add_argument('-f', '--frequency_cutoff', type=float, default=0, help='Minimum frequency of words to consider as possible minimal pairs or contributing to lexicon entropy.')
     parser.add_argument('-r', '--relative_count', type=check_bool, default=True, help='For minimal pair FL: whether or not to divide the number of minimal pairs by the number of possible minimal pairs (words with either segment).')
     parser.add_argument('-d', '--distinguish_homophones', type=check_bool, default=False, help="For minimal pair FL: if False, then you'll count sock~shock (sock=clothing) and sock~shock (sock=punch) as just one minimal pair; but if True, you'll overcount alternative spellings of the same word, e.g. axel~actual and axle~actual. False is the value used by Wedel et al.")
-    parser.add_argument('-y', '--type_or_token', default='token', help='For change in entropy FL: specifies whether entropy is based on type or token frequency.')
+    parser.add_argument('-t', '--type_or_token', default='token', help='For change in entropy FL: specifies whether entropy is based on type or token frequency.')
     parser.add_argument('-e', '--relative_fl', type=check_bool, default=False, help="If True, instead of reading the ")
-    parser.add_argument('-t', '--tier', default='transcription', help="The attribute of Words to calculate FL over. Normally this will be the transcription, but it can also be the spelling or a user-specified tier.")
+    parser.add_argument('-s', '--sequence_type', default='transcription', help="The attribute of Words to calculate FL over. Normally this will be the transcription, but it can also be the spelling or a user-specified tier.")
     parser.add_argument('-o', '--outfile', help='Name of output file')
 
     args = parser.parse_args()
@@ -50,14 +50,14 @@ def main():
 
     if args.algorithm == 'minpair':
         if args.relative_fl:
-            result = relative_minpair_fl(corpus, segpairs_or_segment, frequency_cutoff=args.frequency_cutoff, relative_count=bool(args.relative_count), distinguish_homophones=args.distinguish_homophones, tier=args.tier)
+            result = relative_minpair_fl(corpus, segpairs_or_segment, frequency_cutoff=args.frequency_cutoff, relative_count=bool(args.relative_count), distinguish_homophones=args.distinguish_homophones, sequence_type=args.sequence_type)
         else:
-            result = minpair_fl(corpus, segpairs_or_segment, frequency_cutoff=args.frequency_cutoff, relative_count=bool(args.relative_count), distinguish_homophones=args.distinguish_homophones, tier=args.tier)
+            result = minpair_fl(corpus, segpairs_or_segment, frequency_cutoff=args.frequency_cutoff, relative_count=bool(args.relative_count), distinguish_homophones=args.distinguish_homophones, sequence_type=args.sequence_type)
     elif args.algorithm == 'deltah':
         if args.relative_fl:
-            result = relative_deltah_fl(corpus, segpairs_or_segment, frequency_cutoff=args.frequency_cutoff, type_or_token=args.type_or_token, tier=args.tier)
+            result = relative_deltah_fl(corpus, segpairs_or_segment, frequency_cutoff=args.frequency_cutoff, type_or_token=args.type_or_token, sequence_type=args.sequence_type)
         else:
-            result = deltah_fl(corpus, segpairs_or_segment, frequency_cutoff=args.frequency_cutoff, type_or_token=args.type_or_token, tier=args.tier)
+            result = deltah_fl(corpus, segpairs_or_segment, frequency_cutoff=args.frequency_cutoff, type_or_token=args.type_or_token, sequence_type=args.sequence_type)
     else:
         raise Exception('-a / --algorithm must be set to either \'minpair\' or \'deltah\'.')
 
