10f596b573d0272efc59dc7a441db52a77c1ff8e
kchall <kathleen.hall@ubc.ca>
2015-03-03 17:12:06 -0800

    Cleanup

--- cx_setup.py
+++ cx_setup.py
@@ -90,7 +90,7 @@ bdist_mac_options = {'iconfile':'docs/images/icon.icns',
 bdist_dmg_options = {'applications_shortcut':True}
 
 setup(name='Phonological CorpusTools',
-      version='1.0.0',
+      version='1.0.1',
       description='',
       long_description='',
       classifiers=[
