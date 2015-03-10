9c9fd21e0cbe2debf26cbe98189c569710d106a8
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2015-03-10 15:38:45 -0700

    Fix for khorsi tests failing

--- tests/test_freq_of_alt.py
+++ tests/test_freq_of_alt.py
@@ -29,43 +29,43 @@ def test_freqalt(specified_test_corpus):
                                 phono_align=False)
     assert(result==(8,7,0.875))
 
-    result = calc_freq_of_alt(self.corpus,'s','ʃ','khorsi','type', min_rel = -6,
+    result = calc_freq_of_alt(specified_test_corpus,'s','ʃ','khorsi','type', min_rel = -6,
                                 phono_align=False)
     assert(result==(8,2,0.25))
 
-    result = calc_freq_of_alt(self.corpus,'s','ʃ','khorsi','token', min_rel = -6,
+    result = calc_freq_of_alt(specified_test_corpus,'s','ʃ','khorsi','token', min_rel = -6,
                                 phono_align=False)
     assert(result==(8,3,0.375))
 
-    result = calc_freq_of_alt(self.corpus,'s','ʃ','edit_distance','type', max_rel = 2,
+    result = calc_freq_of_alt(specified_test_corpus,'s','ʃ','edit_distance','type', max_rel = 2,
                                 phono_align=True)
     assert(result==(8,2,0.25))
 
-    result = calc_freq_of_alt(self.corpus,'s','ʃ','edit_distance','token', max_rel = 4,
+    result = calc_freq_of_alt(specified_test_corpus,'s','ʃ','edit_distance','token', max_rel = 4,
                                 phono_align=True)
     assert(result==(8,3,0.375))
 
-    result = calc_freq_of_alt(self.corpus,'s','ʃ','edit_distance','type', max_rel = 2,
-                                phono_align=False, output_filename='nov4tests.txt')
+    result = calc_freq_of_alt(specified_test_corpus,'s','ʃ','edit_distance','type', max_rel = 2,
+                                phono_align=False)
     assert(result==(8,2,0.25))
 
-    result = calc_freq_of_alt(self.corpus,'s','ʃ','edit_distance','token', max_rel = 4,
+    result = calc_freq_of_alt(specified_test_corpus,'s','ʃ','edit_distance','token', max_rel = 4,
                                 phono_align=False)
     assert(result==(8,6,0.75))
 
-    result = calc_freq_of_alt(self.corpus,'s','ʃ','phono_edit_distance','type', max_rel = 6,
+    result = calc_freq_of_alt(specified_test_corpus,'s','ʃ','phono_edit_distance','type', max_rel = 6,
                                 phono_align=True)
     assert(result==(8,2,0.25))
 
-    result = calc_freq_of_alt(self.corpus,'s','ʃ','phono_edit_distance','token', max_rel = 20,
+    result = calc_freq_of_alt(specified_test_corpus,'s','ʃ','phono_edit_distance','token', max_rel = 20,
                                 phono_align=True)
     assert(result==(8,3,0.375))
 
-    result = calc_freq_of_alt(self.corpus,'s','ʃ','phono_edit_distance','type', max_rel = 6,
+    result = calc_freq_of_alt(specified_test_corpus,'s','ʃ','phono_edit_distance','type', max_rel = 6,
                                 phono_align=False)
     assert(result==(8,2,0.25))
 
-    result = calc_freq_of_alt(self.corpus,'s','ʃ','phono_edit_distance','token', max_rel = 20,
+    result = calc_freq_of_alt(specified_test_corpus,'s','ʃ','phono_edit_distance','token', max_rel = 20,
                                 phono_align=False)
     assert(result==(8,6,0.75))
 
