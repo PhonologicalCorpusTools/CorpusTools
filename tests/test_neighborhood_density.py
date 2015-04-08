7173a2492bdb22c5019d50a04ac30b0de6b2b872
Blake Allen <allenblakeh@gmail.com>
2015-04-07 17:18:39 -0700

    fix neighdens test

--- tests/test_neighborhood_density.py
+++ tests/test_neighborhood_density.py
@@ -4,7 +4,7 @@ import os
 
 from corpustools.corpus.classes import Word
 
-from corpustools.neighdens.neighborhood_density import neighborhood_density
+from corpustools.neighdens.neighborhood_density import neighborhood_density, find_mutation_minpairs
 
 
 def test_basic_corpus_nd(unspecified_test_corpus):
@@ -22,10 +22,10 @@ def test_basic_corpus_nd(unspecified_test_corpus):
 
 def test_basic_corpus_mutation_minpairs(unspecified_test_corpus):
     calls = [({'corpus': unspecified_test_corpus,
-                    'query':Word(**{'transcription': ['s', 'ɑ', 't', 'ɑ']},
-                    'max_distance':1},2)]
+                    'query':Word(**{'transcription': ['s', 'ɑ', 't', 'ɑ']}),
+                    },2)]
 
     for c,v in calls:
         result = find_mutation_minpairs(**c)
         assert(result[0] == v)
-        assert(result[1] == 'x')  # also check the identities of the minpairs rather than just their count
+        assert(result[1] == ['n.ɑ.t.ɑ', 'm.ɑ.t.ɑ'] or result[1] == ['m.ɑ.t.ɑ', 'n.ɑ.t.ɑ'])
