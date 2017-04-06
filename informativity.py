2b2982ad1c2af1a5607408c88bc49c53240a41b7
Khia Johnson <khiajohnson@users.noreply.github.com>
2017-04-06 15:05:51 -0700

    Add files via upload

---
+++ informativity.py
@@ -0,0 +1,183 @@
+from math import log2
+from collections import defaultdict
+
+
+def context(index, word):
+    """Get the context for a given segment, specified by its index. The context is 
+    all preceding segments in the word. In the future, functionality to set the number
+    of segments will be added. "All" is the preferred context type in Cohen Priva (2015).
+
+    Parameters
+    ----------
+    index: int 
+        Segment index
+    word: Word 
+        Word object from which the context will be obtained
+
+    Returns
+    -------
+    tuple (Segment, ...)
+        Tuple of context Segments     
+   """
+    return tuple(word.transcription[0:index - 1])
+
+
+def segment_in_context_frequencies(segment, corpus_context):
+    """Gets the frequencies of a segment occurring in a given context
+
+    Parameters
+    ----------
+    segment: str
+    corpus_context: CorpusContext
+        Context manager for a corpus
+
+   Returns
+    ----------
+    dict {tuple : int,...}
+        Dictionary with tuple of context Segments as key, and integer of frequency as value
+    """
+    contexts = defaultdict(int)
+    for word in corpus_context:
+        i = 0
+        for seg in word.transcription:
+            i += 1
+            if seg == segment:
+                contexts[context(i, word)] += word.frequency
+    return contexts
+
+
+def context_frequencies(contexts, corpus_context):
+    """Given a dictionary (or list/iterable) of contexts and a corpus, gets frequencies for the
+     contexts regardless of the following segment.
+
+    Parameters
+    ----------
+    contexts: dict (or other iterable)
+        Dictionary or other iterable of tuples containing contexts
+    corpus_context: CorpusContext
+        Context manager for a corpus
+
+    Returns
+    ----------
+    dict {tuple : int,...}
+        Dictionary with tuple of context segments as key, and integer of frequency as value
+    """
+    context_frs = defaultdict(int)
+    for c in contexts:
+        if c is not None:
+            for word in corpus_context:
+                if tuple(word.transcription[0:len(c)]) == c:
+                    context_frs[c] += word.frequency
+    return context_frs
+
+
+def conditional_probability(segment_frs, context_frs):
+    """Gets the conditional probability of a segment occurring in a given context.
+
+    Parameters
+    ----------
+    segment_frs: dict {Segment : int, ...}
+        Dictionary of segment frequencies, specifically {Segment : frequency of segment|context ,...}
+    context_frs: dict {Segment : int, ...}
+        Dictionary of context frequencies, specifically {Segment : frequency of context|segment ,...}
+
+    Returns
+    ----------
+    dict {tuple:float,...}
+        Dictionary with tuple of context Segments as key, and float of conditional probability
+         of the given Segment occurring in that context
+    """
+    conditional_prs = defaultdict(float)
+    for c in segment_frs:
+        if context_frs[c] != 0:
+            conditional_prs[c] = segment_frs[c] / context_frs[c]
+        else:
+            conditional_prs[c] = segment_frs[c]
+    return conditional_prs
+
+
+def get_informativity(corpus_context, segment, rounding=3):
+    """Calculate the informativity of one segment. 
+
+    Parameters
+    ----------
+    corpus_context: CorpusContext
+        Context manager for a corpus
+    segment: Segment 
+        The Segment for which informativity should be calculated
+    rounding: int 
+        Integer indicates the number of decimal places
+
+    Returns
+    ----------
+    dict 
+        Dictionary provides a summary of the parameters, with string keys. Value
+        for informativity is a float with specified rounding
+    """
+    s_frs = segment_in_context_frequencies(segment, corpus_context)
+    c_frs = context_frequencies(s_frs, corpus_context)
+    c_prs = conditional_probability(s_frs, c_frs)
+    informativity = round(-(sum([(s_frs[c]) * log2(c_prs[c]) for c in c_prs])) / sum([(s_frs[s]) for s in s_frs]),
+                          rounding)
+
+    summary = {
+        "Corpus": corpus_context.name,
+        "Segment": segment,
+        "Context": "all",
+        "Rounding": rounding,
+        "Informativity": informativity
+    }
+    return summary
+
+
+def all_informativity(corpus_context, rounding=3):
+    """
+
+    Parameters
+    ----------
+    corpus_context: CorpusContext
+        Context manager for a corpus
+    rounding: int 
+        Integer indicates the number of decimal places
+
+    Returns
+    ----------
+    dict {Segment: dict, ...}
+        Dictionary of dictionaries containing informativity summaries, 
+        with Segment objects as keys. 
+    """
+    all_informativities = defaultdict(dict)
+    for segment in corpus_context.inventory:
+        all_informativities[str(segment)] = get_informativity(corpus_context, segment, rounding)
+    return all_informativities
+
+
+# ------------------------------------------------------------------#
+# TESTING
+# The following code will not be included in the actual PCT release,
+# and is here for the purposes of testing for the LIBR 559C project.
+# All testing is done with the Lemurian corpus - lemurian.corpus
+# ------------------------------------------------------------------#
+
+# Additional imports for testing purposes
+import pickle
+from corpustools.corpus.classes import Segment
+
+# Load the corpus
+corpus_path = "lemurian.corpus"
+with open(corpus_path, 'rb') as file:
+    corpus_in = pickle.load(file)
+
+# Corpus information
+print("Corpus: ", corpus_in.name)
+print("Segments in the lemurian corpus: ", [seg for seg in corpus_in.inventory], "\n")
+
+# Informativity of a single segment
+print("Informativity for one segment:")
+print(get_informativity(corpus_in, Segment("m"), rounding=3), "\n")
+
+# Informativity for all segments in inventory
+print("Informativity for all segments in inventory:")
+output = all_informativity(corpus_in, rounding=4)
+for s in sorted(output.keys()):
+    print(s, output[s])
---
+++ informativity.rst
@@ -0,0 +1,120 @@
+.. _informativity:
+
+*************
+Informativity
+*************
+
+.. _about_informativity:
+
+About the function
+------------------
+
+Informativity is one of three primary information theoretic measures that has been demonstrated to affect how speech is
+realized. Informativity is “the weighted average of the negative log predictability of all the occurrences of a segment”
+[CohenPriva2015]_. It is formally defined with the following formula:
+
+:math:`-sum[P(context|segment) * log2P(segment|context)]`
+
+While other information theoretic measures such as frequency and predictability can be used to describe a wide variety
+of deletion and duration effects, Cohen Priva argues that informativity provides more nuanced and accurate predictions,
+and offers empirical evidence from English, as well as other languages [CohenPrivaInPress]_.
+
+One of the primary decisions in calculating informativity, is selecting the type of context to be used.
+[CohenPriva2015]_ discusses four options used in the literature - uniphone, biphone, triphone, and all preceding
+segments in the word. Within PCT, "all preceding" is currently the only supported option.
+
+.. _method_informativity:
+
+Method of calculation
+---------------------
+
+.. _method_context:
+
+Defining context
+````````````````
+The context for a given segment is currently defined as all of the preceding segments within the same word - the
+preferred method in [CohenPriva2015]. The context method includes parameters for the index (integer) of the segment and word in question. Index is used instead of segment, as a word may contain more than one of the same segment,and it is important to consider the context for each occurrence. The function returns a tuple of segments comprising the context. Typical users will not interact with context. Future improvements to the informativity function will allow for customizable context.
+
+Informativity
+`````````````
+The function to get the informativity of one segment is structured such that it calls on other functions within
+:math:`informativity.py` to create three dictionaries containing:
+
+1.  The frequency of a segment occurring given a context, with contexts as the key and captured in the dictionary
+:math:`s_frs`
+2.  The frequency of those contexts, regardless of the segment that occurs after
+3.  The conditional probabilities of a segment occurring in a given context, captured in dictionary :math:`c_prs`.
+
+Given this input, the informativity of a given segment is calculated as follows:
+
+:math:`informativity=round(-(sum([(s_frs[c])*log2(c_prs[c]) for c in c_prs]))/sum([(s_frs[s])for s in s_frs]),rounding)`
+
+The following is an example run of the function for a single segment:
+
+.. image:: static/informativity1.png
+   :width: 90%
+   :align: center
+
+In addition to getting the informativity for a single segment, :math:`informativity.py` includes a function to calculate
+the informativity of all segments in a corpus. This function gets the list of segments from the corpus’ inventory, and
+creates a dictionary with the segments as the key, and the output of the get_informativity function as its value.
+
+The following is an example run for getting the informativities for all segments in the inventory:
+
+.. image:: static/informativity2.png
+   :width: 90%
+   :align: center
+
+.. _informativity_corpus_file:
+
+Calculating informativity with a .corpus file
+---------------------------------------------
+
+1.  **Locate the corpus**: Verify that the lemurian.corpus file is located in the same directory as informativity.py.
+
+2.  **Run informativity.py**: Open a terminal and navigate to the directory where informativity.py is located. Note that
+PCT uses Python 3, and run the following:
+
+    :math:`python informativity.py`
+
+The following is an example run of the current test print statements:
+
+.. image:: static/informativity_559tests.png
+   :width: 90%
+   :align: center
+
+3.  **Run additional tests**: At your discretion!
+
+
+**NOTE**: In the future, this portion of the documentation will be modified for calculating informativity in the GUI and
+on the command line, to better conform to and integrate with PCT.
+
+.. _functional_load_gui:
+
+Calculating functional load in the GUI
+--------------------------------------
+Details will be added here upon full integration with PCT.
+
+.. _functional_load_cli:
+
+Implementing the functional load function on the command line
+-------------------------------------------------------------
+Details will be added here upon full integration with PCT.
+
+.. _informativity_classes_and_functions:
+
+Additional Information
+----------------------
+Details will be added here upon full integration with PCT.
+
+**********
+References
+**********
+
+Note that these references will be migrated to the "references.rst" file when fully integrated.
+
+.. [CohenPriva2015] Cohen Priva, Uriel (2015). Informativity affects consonant duration and deletion rates. Laboratory
+Phonology, 6(2), 243–278.
+
+.. [CohenPrivaInPress] Cohen Priva, Uriel (in press). Informativity and the actuation of lenition. Language. Retrieved
+from: https://urielcpublic.s3.amazonaws.com/Informativity-and-the-actuation-of-lenition-accepted.pdf
