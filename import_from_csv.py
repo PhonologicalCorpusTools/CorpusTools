2eae69774b93b65b8810bcada035f6fdc0b4d516
jsmackie <mr.jsmackie@gmail.com>
2017-06-13 13:09:53 -0700

    Remove extra files

--- import_from_csv.py
+++
@@ -1,8 +0,0 @@
-import corpustools.corpus.io.csv as csv
-import os
-
-name = 'LF'
-path = os.path.join(os.getcwd(), 'LF_closed_monosyllables.txt')
-delim = '\t'
-
-corpus = csv.load_corpus_csv(name, path, delim)
