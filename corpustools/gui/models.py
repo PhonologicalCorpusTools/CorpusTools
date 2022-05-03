f3e37e39f0c1b103ce3d8cb13262df07c2c2385a
stannam <stan.nam98@gmail.com>
2022-05-03 00:58:49 -0700

    Bypasses the raw_env check if list by segment re: #801 This change partially undoes f359d5e4169d1ae8b465ba006f0308fa67b6393f where I made the summary result to have a 0 frequency row when the search has no hit. The introduction of 0 frequency rows was due to the new way of summary presentation. It is not relevant to the old way.

--- corpustools/gui/models.py
+++ corpustools/gui/models.py
@@ -591,7 +591,7 @@ class PhonoSearchResultsModel(BaseTableModel):
                     pass
                 segenvfilters = seg, envs[i], filters, tier, res_type  # segs + envs + (freq and phoneme/syllable count filters) + result_type
                 if res_type == 'positive':  # if positive search
-                    if line['raw_env'][i] is not None:      # then check if the word is in results for satisfying env[i]
+                    if line['raw_env'][i] is not None or segsum:  # then check if the word is in results for satisfying env[i]
                         typefreq[segenvfilters] += 1        # if so, +1 in the type freq
                         tokenfreq[segenvfilters] += line['Word'].frequency  # and add token freq accordingly
                     else:
